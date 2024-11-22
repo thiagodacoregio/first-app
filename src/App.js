739828a8a8c297db780e66b51d138fe2a92ea431
Thiago Da Coregio <thiagodacoregio@gmail.com>
2024-11-22 22:44:31 +0000

    my first-react-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [task, setTask] = useState([]);
+
+  // Recupera os dados do localStorage ao carregar a página
+  useEffect(() => {
+    const taskStorage = localStorage.getItem("@task");
+    if (taskStorage) {
+      setTask(JSON.parse(taskStorage));
+    }
+  }, []);
+
+  // Salva as tarefas no localStorage sempre que o array "task" for atualizado
+  useEffect(() => {
+    if (task.length > 0) {
+      localStorage.setItem("@task", JSON.stringify(task));
+    }
+  }, [task]);
+
+  // Adiciona uma nova tarefa
+  function handleRegister(e) {
+    e.preventDefault();
+    if (input.trim() === "") {
+      alert("Please, write a task before submitting!");
+      return;
+    }
+    setTask([...task, input.trim()]);
+    setInput("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Task Manager</h1>
+      <form onSubmit={handleRegister}>
+        <label>Task name:</label>
+        <br />
+        <input
+          placeholder="Write one task"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+
+      <br />
+      <br />
+
+      <ul>
+        {task.map((getTask, index) => (
+          <li key={index}>{getTask}</li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/components/Name.js
@@ -0,0 +1,9 @@
+function Name({ student, age }) {
+  return (
+    <span>
+      Wellcome: {student} - Age: {age} years old
+    </span>
+  );
+}
+
+export default Name;
